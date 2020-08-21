3bad73a9ef032ff1abb8daca9eb62c7f77976371
Panda Foss <maxi.fg13@gmail.com>
2020-08-20 21:30:54 -0300

    My solutions

---
+++ exercises/clippy/target/debug/deps/clippy2-5fb0979f9b04da90.d
@@ -0,0 +1,5 @@
+/home/masito/Programacion/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-5fb0979f9b04da90.rmeta: clippy2.rs
+
+/home/masito/Programacion/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-5fb0979f9b04da90.d: clippy2.rs
+
+clippy2.rs:
---
+++ exercises/tests/tests2.rs
@@ -0,0 +1,13 @@
+// tests2.rs
+// This test has a problem with it -- make the test compile! Make the test
+// pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn you_can_assert_eq() {
+        let numero = 11;
+        assert_eq!(numero, 11);
+    }
+}
